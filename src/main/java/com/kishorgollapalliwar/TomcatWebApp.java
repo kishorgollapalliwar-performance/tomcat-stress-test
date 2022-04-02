040376cd1a30975439a0e711591a7ba80a87d41d
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2022-04-02 19:05:05 +0530

    Created class which is responsible for starting tomcat

---
+++ src/main/java/com/kishorgollapalliwar/TomcatWebApp.java
@@ -0,0 +1,28 @@
+package com.kishorgollapalliwar;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class SimpleWebApp {
+	public void start() {
+		Tomcat tomcat = setupTomcat();
+	}
+
+    private Tomcat setupTomcat() {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir("temp");
+        tomcat.setPort(8080);
+
+        return tomcat;
+    }
+}
