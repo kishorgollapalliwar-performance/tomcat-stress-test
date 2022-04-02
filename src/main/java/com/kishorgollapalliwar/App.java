4d4596d21b3057db470206d27a10c6a32da8c181
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2022-04-02 11:11:57 +0530

    [Initial] Create maven quickstart java project

---
+++ src/main/java/com/kishorgollapalliwar/App.java
@@ -0,0 +1,13 @@
+package com.kishorgollapalliwar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
