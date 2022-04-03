315381c1986eb97ed38e136ad755f380e9d19e28
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2022-04-03 19:41:41 +0530

    [TomcatWebApp] Added time servlet to tomcat web app

--- src/main/java/com/kishorgollapalliwar/TomcatWebApp.java
+++ src/main/java/com/kishorgollapalliwar/TomcatWebApp.java
@@ -1,6 +1,13 @@
 package com.kishorgollapalliwar;
 
 import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
@@ -10,9 +17,11 @@ import org.apache.catalina.startup.Tomcat;
  * This class is responsible to manage life cycle of tomcat server
  **/
 public class TomcatWebApp {
-	    public void start() {
+	public void start() {
         Tomcat  tomcat  = setupTomcat();
         Context context = setupContext(tomcat);
+
+		addTimeServlet(tomcat, context);
 
         try {
             tomcat.start();
@@ -34,5 +43,20 @@ public class TomcatWebApp {
         String contextPath = "/";
         String docBase     = new File(".").getAbsolutePath();
         return tomcat.addContext(contextPath, docBase);
+    }
+
+    private void addTimeServlet(final Tomcat  tomcat, final Context context) {
+        HttpServlet timeServlet = new HttpServlet() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                resp.getWriter().println(LocalDateTime.now());
+            }
+        };
+
+        tomcat.addServlet("/", "TimeServlet", timeServlet);
+        context.addServletMappingDecoded("/server-time", "TimeServlet");
+        context.addServletMappingDecoded("/", "TimeServlet");
     }
 }
