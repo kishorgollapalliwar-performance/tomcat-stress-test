3d86bdfe126be22d622c0a5d3372a432d962fd0e
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2022-04-03 19:34:15 +0530

    [App] Added code to start TomcatWebApp

--- src/main/java/com/kishorgollapalliwar/App.java
+++ src/main/java/com/kishorgollapalliwar/App.java
@@ -4,10 +4,9 @@ package com.kishorgollapalliwar;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        TomcatWebApp simpleWebApp = new TomcatWebApp();
+        simpleWebApp.start();
     }
 }
