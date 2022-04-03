80341fb500ea39b0d818aa73b88fbadc5b386f1c
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2022-04-03 19:32:18 +0530

    [TomcatWebApp] Added tomcat setup methods

--- src/main/java/com/kishorgollapalliwar/TomcatWebApp.java
+++ src/main/java/com/kishorgollapalliwar/TomcatWebApp.java
@@ -1,14 +1,7 @@
 package com.kishorgollapalliwar;
 
 import java.io.File;
-import java.io.IOException;
-import java.time.LocalDateTime;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
- 
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
@@ -16,11 +9,18 @@ import org.apache.catalina.startup.Tomcat;
 /**
  * This class is responsible to manage life cycle of tomcat server
  **/
-public class SimpleWebApp {
-	public void start() {
-		Tomcat tomcat   = setupTomcat();
-		Context context = setupContext(tomcat);
-	}
+public class TomcatWebApp {
+	    public void start() {
+        Tomcat  tomcat  = setupTomcat();
+        Context context = setupContext(tomcat);
+
+        try {
+            tomcat.start();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
+        tomcat.getServer().await();
+    }
 
     private Tomcat setupTomcat() {
         Tomcat tomcat = new Tomcat();
